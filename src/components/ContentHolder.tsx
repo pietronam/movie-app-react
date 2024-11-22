536123e7b2fd323d34839720ed0a1db593887464
nam <pietronam.cavallanti@gmail.com>
2024-11-22 10:41:41 +0100

    Aggiunta griglia

--- src/components/ContentHolder.tsx
+++ src/components/ContentHolder.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { ContentType } from "../types/contentTypes";
 import { getTrendingContent } from "../api/content";
 import { ContentCard } from "./ContentCard";
+import { SimpleGrid } from "@chakra-ui/react";
 
 type ContentHolderProps = {
     pName: string,
@@ -21,21 +22,23 @@ export const ContentHolder = ({ url, pName }: ContentHolderProps) => {
     return (
         <>
             <h2>{pName}</h2>
-            <div style={{ width: "100%" }}>
-                {
-                    content.map(
-                        (content) => {
-                            switch (content.media_type) {
-                                case "movie":
-                                    return <ContentCard pClassName={"movie-card"} key={content.id} content={content}/>
-                                case "tv":
-                                    return <ContentCard pClassName={"tv-card"} key={content.id} content={content} />
-                                case "person":
-                                    return <ContentCard pClassName={"person-card"} key={content.id} content={content} />
-                            }
-                        })
-                }
-            </div>
+            <SimpleGrid columns={3} gap={40}>
+                
+                    {
+                        content.map(
+                            (content) => {
+                                switch (content.media_type) {
+                                    case "movie":
+                                        return <ContentCard pClassName={"movie-card"} key={content.id} content={content} />
+                                    case "tv":
+                                        return <ContentCard pClassName={"tv-card"} key={content.id} content={content} />
+                                    case "person":
+                                        return <ContentCard pClassName={"person-card"} key={content.id} content={content} />
+                                }
+                            })
+                    }
+                
+            </SimpleGrid>
         </>
     )
 }
